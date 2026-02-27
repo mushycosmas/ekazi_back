0c44d8b94760879375a8b5b367e472162b683b90
mushycosma <kevomassawe@gmail.com>
2026-02-27 13:07:22 +0300

    fix:data

--- src/cvbuilder/cvbuilder.service.ts
+++ src/cvbuilder/cvbuilder.service.ts
@@ -12,80 +12,65 @@ export class CvbuilderService {
   ) {}
 
   async getApplicantCv(applicantId: number) {
-    const applicant = await this.applicantsRepo.findOne({
-      where: { id: applicantId },
-      relations: [
-        'user',
-        'marital',
-        'gender',
-        'applicant_career',
-        'applicant_tools',
-        'applicant_tools.tools', // tools relation
-        'applicant_phones',
-        'referees',
-        'applicant_cultures',
-        'applicant_cultures.culture', // culture relation
-        'applicant_knowledge',
-        'applicant_knowledge.knowledge', // knowledge relation
-      ],
-    });
+    const applicant = await this.applicantsRepo
+      .createQueryBuilder('applicant')
+      .leftJoinAndSelect('applicant.user', 'user')
+      .leftJoinAndSelect('applicant.marital', 'marital')
+      .leftJoinAndSelect('applicant.gender', 'gender')
+      .leftJoinAndSelect('applicant.applicant_career', 'career')
+      .leftJoinAndSelect('applicant.applicant_tools', 'applicantTools')
+      .leftJoinAndSelect('applicantTools.tools', 'tools')
+      .leftJoinAndSelect('applicant.applicant_phones', 'phones')
+      .leftJoinAndSelect('applicant.referees', 'referees')
+      .leftJoinAndSelect('applicant.applicant_cultures', 'applicantCultures')
+      .leftJoinAndSelect('applicantCultures.culture', 'culture')
+      .leftJoinAndSelect('applicant.applicant_knowledge', 'applicantKnowledge')
+      .leftJoinAndSelect('applicantKnowledge.knowledge', 'knowledge')
+      // Pick only the columns you want
+      .select([
+        'applicant.id',
+        'applicant.first_name',
+        'applicant.middle_name',
+        'applicant.last_name',
+        'applicant.dob',
+        'applicant.nationality_id',
+        'applicant.picture',
+        'applicant.background_picture',
+
+        'user.email',
+        'marital.id',
+        'marital.marital_status',
+
+        'gender.id',
+        'gender.gender_name',
+
+        'career.career',
+
+        'tools.id',
+        'tools.tool_name',
+        'applicantTools.tool_id',
+
+        'phones.phone_number',
+
+        'referees.first_name',
+        'referees.middle_name',
+        'referees.last_name',
+        'referees.employer',
+        'referees.referee_position',
+        'referees.email',
+        'referees.phone',
+
+        'culture.id',
+        'culture.culture_name',
+
+        'knowledge.id',
+        'knowledge.knowledge_name',
+      ])
+      .where('applicant.id = :id', { id: applicantId })
+      .getOne();
 
     if (!applicant) return null;
 
-    return {
-      id: applicant.id,
-      first_name: applicant.first_name,
-      middle_name: applicant.middle_name,
-      last_name: applicant.last_name,
-      dob: applicant.dob,
-      status_profile: applicant.status_profile,
-      nationality_id: applicant.nationality_id,
-      picture: applicant.picture,
-      background_picture: applicant.background_picture,
-      marital: applicant.marital
-        ? { marital_status: applicant.marital.marital_status }
-        : null,
-      gender: applicant.gender
-        ? { gender_name: applicant.gender.gender_name }
-        : null,
-      user: applicant.user
-        ? {
-            id: applicant.user.id,
-            role_id: applicant.user.role_id,
-            username: applicant.user.username,
-            email: applicant.user.email,
-            verified: applicant.user.verified,
-            temp_email: applicant.user.temp_email,
-            last_activity_at: applicant.user.last_activity_at,
-          }
-        : null,
-      applicant_career: applicant.applicant_career?.map((c) => ({
-        career: c.career,
-      })),
-      applicant_tools: applicant.applicant_tools?.map((t) => ({
-        tool_id: t.tool_id,
-        tool_name: t.tools?.tool_name,
-      })),
-      applicant_phones: applicant.applicant_phones?.map((p) => ({
-        phone_number: p.phone_number,
-      })),
-      referees: applicant.referees?.map((r) => ({
-        first_name: r.first_name,
-        middle_name: r.middle_name,
-        last_name: r.last_name,
-        employer: r.employer,
-        referee_position: r.referee_position,
-        email: r.email,
-        phone: r.phone,
-      })),
-      applicant_cultures: applicant.applicant_cultures?.map((c) => ({
-        culture_id: c.culture_id,
-        culture_name: c.culture?.culture_name
-      })),
-      applicant_knowledge: applicant.applicant_knowledge?.map((k) => ({
-        knowledge_id: k.knowledge_id,
-        knowledge_name: k.knowledge?.knowledge_name,
-      })),
-    };
+    return applicant;
   }
 }
