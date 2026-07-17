aa6bfaa0555efb4a034b91cf742777bd7434efd9
HalidiManeno <halidiselemani94@gmail.com>
2026-07-17 09:00:50 +0300

    update job settings

--- src/jobs/controllers/job-settings.controller.ts
+++ src/jobs/controllers/job-settings.controller.ts
@@ -1,22 +1,29 @@
-import { Body, Controller, Param, Put, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  ParseIntPipe,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { SanctumGuard } from 'src/auth/guards/sanctum.guard';
 import { UpdateJobSettingsDto } from '../dtos/update-job-settings.dto';
 import { JobSettingsService } from '../services/job-settings.service';
 
 @Controller('employer/jobs')
 export class JobSettingsController {
-      constructor(
+  constructor(
     private readonly jobSettingsService: JobSettingsService,
   ) {}
 
-  @Put('job-settings/:id')
+  @Put(':id/job-settings')
   @UseGuards(SanctumGuard)
-  updateSettings(
-    @Param('id') id: string,
+  async updateSettings(
+    @Param('id', ParseIntPipe) id: number,
     @Body() dto: UpdateJobSettingsDto,
   ) {
     return this.jobSettingsService.updateSettings(
-      Number(id),
+      id,
       dto,
     );
   }
